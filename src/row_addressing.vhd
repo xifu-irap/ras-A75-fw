6cf38bb1fb8c81890e8b3740b9f2bc001c101627
LaurentRavera <laurent.ravera@irap.omp.eu>
2022-08-30 14:02:47 +0200

    fw-0015: to measure sys_clk and o_clk frequencies

--- src/row_addressing.vhd
+++ src/row_addressing.vhd
@@ -305,6 +305,7 @@ signal cmp : unsigned(25 downto 0);
 --signal i_clk : std_logic ;
 signal clk_200M : std_logic ; 
 signal sys_clk : std_logic ; -- 245.76 MHz
+signal sys_clkb : std_logic ; -- 245.76 MHz
 signal oo_clk : std_logic ; -- output clock at 100 MHz
 signal CLKFBOUT : std_logic ; 
 signal LOCKED : std_logic;
@@ -323,7 +324,7 @@ begin
 --=========================================================
 
 ------- TO BE CHANGED ACCORDING TO THE FIRMWARE VERSION -----
-Version.Firmware_id <= x"0014";
+Version.Firmware_id <= x"0015";
 ------- TO BE CHANGED ACCORDING TO THE HARDWARE VERSION -----
 Version.RAS_board_id <= x"0000";
 -------------------------------------------------------------
@@ -390,7 +391,7 @@ s_rst <= rst_gen(rst_gen'left);
 MMCME2_BASE_inst : MMCME2_BASE
 generic map (
    BANDWIDTH => "OPTIMIZED",  -- Jitter programming (OPTIMIZED, HIGH, LOW)
-   CLKFBOUT_MULT_F => 7.68,    -- Multiply value for all CLKOUT (2.000-64.000).
+   CLKFBOUT_MULT_F => 49.0,    -- Multiply value for all CLKOUT (2.000-64.000).
    CLKFBOUT_PHASE => 0.0,     -- Phase offset in degrees of CLKFB (-360.000-360.000).
    CLKIN1_PERIOD => 5.0,      -- Input clock period in ns to ps resolution (i.e. 33.333 is 30 MHz).
    -- CLKOUT0_DIVIDE - CLKOUT6_DIVIDE: Divide amount for each CLKOUT (1-128)
@@ -400,7 +401,7 @@ generic map (
    CLKOUT4_DIVIDE => 1,
    CLKOUT5_DIVIDE => 1,
    CLKOUT6_DIVIDE => 1,
-   CLKOUT0_DIVIDE_F => 3.125,   -- Divide amount for CLKOUT0 (1.000-128.000).
+   CLKOUT0_DIVIDE_F => 3.625,   -- Divide amount for CLKOUT0 (1.000-128.000).
    -- CLKOUT0_DUTY_CYCLE - CLKOUT6_DUTY_CYCLE: Duty cycle for each CLKOUT (0.01-0.99).
    CLKOUT0_DUTY_CYCLE => 0.5,
    CLKOUT1_DUTY_CYCLE => 0.5,
@@ -418,14 +419,14 @@ generic map (
    CLKOUT5_PHASE => 0.0,
    CLKOUT6_PHASE => 0.0,
    CLKOUT4_CASCADE => FALSE,  -- Cascade CLKOUT4 counter with CLKOUT6 (FALSE, TRUE)
-   DIVCLK_DIVIDE => 2,        -- Master division value (1-106)
+   DIVCLK_DIVIDE => 11,        -- Master division value (1-106)
    REF_JITTER1 => 0.0,        -- Reference input jitter in UI (0.000-0.999).
    STARTUP_WAIT => FALSE      -- Delays DONE until MMCM is locked (FALSE, TRUE)
 )
 port map (
    -- Clock Outputs: 1-bit (each) output: User configurable clock outputs
    CLKOUT0 => sys_clk,     -- 1-bit output: CLKOUT0
-   CLKOUT0B => open,   -- 1-bit output: Inverted CLKOUT0
+   CLKOUT0B => sys_clkb,   -- 1-bit output: Inverted CLKOUT0
    CLKOUT1 => open,     -- 1-bit output: CLKOUT1
    CLKOUT1B => open,   -- 1-bit output: Inverted CLKOUT1
    CLKOUT2 => open,     -- 1-bit output: CLKOUT2
@@ -1027,7 +1028,9 @@ o_cluster_spare_1 <= sig_overlap13_int;
         o_sig_late => sig_overlap15_int
     );
 
-    o_synchro <= sig_overlap15_int;
+--- Just for testing
+  --  o_synchro <= sig_overlap15_int;
+    o_synchro <= sys_clkb;
 
 ----
 fifoOut_din <= sig_overlap12_int & sig_overlap11_int & sig_overlap10_int & sig_overlap9_int & sig_overlap8_int & sig_overlap7_int & sig_overlap6_int & sig_overlap5_int & sig_overlap4_int & sig_overlap3_int & sig_overlap2_int & sig_overlap1_int & sig_overlap0_int;         
